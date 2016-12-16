f2d3ff27df3692cfac1e2dadf81e7a7ff20b7a48
Julius Breckel <julius.breckel@signavio.com>
2016-12-16 23:28:36 +0300

    Add markdown previewer

--- src/projects/index.js
+++ src/projects/index.js
@@ -1,9 +1,15 @@
 import TributePage from './tribute-page'
+import MarkdownPreviewer from './markdown-previewer'
 
 export default [
   {
     name: 'Tribute Page',
     Component: TributePage,
     pattern: '/tribute-page',
+  },
+  {
+    name: 'Markdown Previewer',
+    Component: MarkdownPreviewer,
+    pattern: '/markdown-previewer',
   },
 ]
---
+++ src/projects/markdown-previewer/index.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import {
+  Grid,
+  Row,
+  Col,
+} from 'react-bootstrap'
+
+import { compose, mapProps, withState } from 'recompose'
+
+import marked from 'marked'
+
+const Root = ({ rawText, markdown, setText, disabled }) => (
+  <Grid
+    fluid
+  >
+    <Row style={{ height: '100%' }}>
+      <Col md={6}>
+        <textarea
+          disabled={ disabled }
+          rows="22"
+          style={{
+            overflow: disabled && 'hidden',
+            width: '100%',
+            height: '100%',
+            resize: 'vertical',
+          }}
+          value={rawText}
+          onChange={({ target }) => setText(target.value)}
+        />
+      </Col>
+      <Col md={6}>
+        <span dangerouslySetInnerHTML={ markdown } />
+      </Col>
+    </Row>
+  </Grid>
+)
+
+const initialText = `Heading
+=======
+
+Sub-heading
+-----------
+
+### Another deeper heading
+
+Paragraphs are separated
+by a blank line.
+
+Leave 2 spaces at the end of a line to do a
+line break
+
+Text attributes *italic*, **bold**,
+\`monospace\`, ~~strikethrough~~ .
+
+\`\`\`
+console.log('Hello');
+\`\`\`
+
+Shopping list:
+
+  * apples
+  * oranges
+  * pears
+
+Numbered list:
+
+  1. apples
+  2. oranges
+  3. pears
+
+The rain---not the reign---in
+Spain.
+
+ *[Herman Fassett](https://freecodecamp.com/hermanfassett)*
+`
+
+const MarkdownPreviewer = compose(
+  withState('rawText', 'setText', initialText ),
+  mapProps(({ rawText, ...rest }) => ({
+    ...rest,
+    rawText,
+    markdown: { __html: marked(rawText) },
+  })),
+)(Root)
+
+export default MarkdownPreviewer
