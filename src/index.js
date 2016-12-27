dd9338e698cfa6a964b399bd66c347adc9b5e3b3
Julius Breckel <julius.breckel@signavio.com>
2016-12-27 16:57:35 +0300

    Add smoothscroll-polyfill to ensure scrolling is always smooth

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
+require('smoothscroll-polyfill').polyfill()
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
