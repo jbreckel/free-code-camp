0a0f9afb52e4181a89247e9bd832f62f6b3fdd44
Julius Breckel <julius.breckel@signavio.com>
2016-12-31 00:34:31 +0300

    Make links in contact section working

--- src/portfolio/ContactSection.js
+++ src/portfolio/ContactSection.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import { Link } from 'react-router'
-
 import { Section } from '../components'
 
 const ContactSection = () => (
@@ -9,20 +7,21 @@ const ContactSection = () => (
     If you want to contact me, please feel free to send me a carrier pigeon.
     <br />
     If you don&apos;t have your pigeon at hand, you can find me at{' '}
-    <Link
+    <a
       target="_blank"
       rel="noopener noreferrer"
-      to="https://www.linkedin.com/in/jbreckel"
+      href="https://www.linkedin.com/in/jbreckel"
     >
       <i className="fa fa-linkedin-square fa-2x" style={{ color: '#444' }} />
-    </Link> and{' '}
-    <Link
+    </a>
+    {' '}and{' '}
+    <a
       target="_blank"
       rel="noopener noreferrer"
-      to="https://github.com/jbreckel"
+      href="https://github.com/jbreckel"
     >
       <i className="fa fa-github-square fa-2x" style={{ color: '#444' }} />
-    </Link>.
+    </a>.
   </Section>
 )
 
