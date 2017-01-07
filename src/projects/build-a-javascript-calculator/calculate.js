bfc6b6ad713aa2163702fff64179010aea732d13
Julius Breckel <julius.breckel@signavio.com>
2017-01-07 23:48:33 +0300

    add basic calculation logic
    Missing: prevent multiple leading 0 and multiple dots

--- src/projects/build-a-javascript-calculator/calculate.js
+++ src/projects/build-a-javascript-calculator/calculate.js
@@ -1,11 +1,6 @@
-export const add = (x, y) => x + y
-export const subtract = (x, y) => x - y
-export const multiply = (x, y) => x * y
-export const divide = (x, y) => x / y
-
 export default {
-  add,
-  subtract,
-  multiply,
-  divide,
+  '+': (x, y) => x + y,
+  '-': (x, y) => x - y,
+  '*': (x, y) => x * y,
+  '/': (x, y) => x / y,
 }
--- src/projects/build-a-javascript-calculator/index.js
+++ src/projects/build-a-javascript-calculator/index.js
@@ -1,51 +1,44 @@
 import React from 'react'
 
-import { compose, getContext, mapProps, withState } from 'recompose'
+import { compose, getContext, withState, withHandlers } from 'recompose'
 
 import substyle from 'substyle'
 
-import { add } from './calculate'
+import './style.css'
 
-const Col = ({ children }) => (
-  <div
-    style={{
-      // width: '20%',
-      // display: 'flex',
-      // flexDirection: 'column',
-      // justifyContent: 'center',
-      // alignItems: 'center',
-    }}
-  >
+import calculate from './calculate'
+
+const Screen = ({ children, double, sbstyle }) => (
+  <button { ...sbstyle('screen') }>
     { children }
-  </div>
+  </button>
 )
-const Row = ({ children }) => (
-  <div
-    style={{
-      // display: 'flex',
-      // alignItems: 'stretch',
-      // width: '100%',
-    }}
-  >
-    { children }
-  </div>
-)
-const Tile = ({ children, double, sbstyle }) => (
+const Tile = ({ children, double, sbstyle, onClick }) => (
   <button
+    onClick={ onClick }
     style={{
       ...sbstyle('tile').style,
-      width: double ? sbstyle('tile').style.width * 2 : sbstyle('tile').style.width,
+      width: double
+        ? (sbstyle('tile').style.width * 2) + 15
+        : sbstyle('tile').style.width,
     }}
   >
     { children }
   </button>
 )
 
-const JavaScriptCalculator = ({ appColor, style, result, ...props }) => (
+const JavaScriptCalculator = ({
+  appColor, style, result,
+  setOperator,
+  value, addToValue,
+  triggerCalculation,
+  clear,
+}) => (
   <div
     style={{
       ...style('outer').style,
-      backgroundColor: appColor,
+      background: appColor,
+      backgroundSize: 'cover',
     }}
   >
     <div
@@ -59,67 +52,26 @@ const JavaScriptCalculator = ({ appColor, style, result, ...props }) => (
           padding: 10,
         }}
       >
-        <Col>
-          <Row>
-            <Tile sbstyle={ style } >7</Tile>
-            <Tile sbstyle={ style } >8</Tile>
-            <Tile sbstyle={ style } >9</Tile>
-          </Row>
-          <Row>
-            <Tile sbstyle={ style } >4</Tile>
-            <Tile sbstyle={ style } >5</Tile>
-            <Tile sbstyle={ style } >6</Tile>
-          </Row>
-          <Row>
-            <Tile sbstyle={ style } >1</Tile>
-            <Tile sbstyle={ style } >2</Tile>
-            <Tile sbstyle={ style } >3</Tile>
-          </Row>
-          <Row>
-            <Tile double sbstyle={ style } >0</Tile>
-            <Tile sbstyle={ style } >.</Tile>
-          </Row>
-        </Col>
-        <Col>
-          <Row>
-            <Tile sbstyle={ style } >+</Tile>
-          </Row>
-          <Row>
-            <Tile sbstyle={ style } >-</Tile>
-          </Row>
-          <Row>
-            <Tile sbstyle={ style } >*</Tile>
-          </Row>
-          <Row>
-            <Tile sbstyle={ style } >/</Tile>
-          </Row>
-        </Col>
-        {/* <input type="button" value="1" onClick="document.calculator.ans.value+='1'" />
-        <input type="button" value="2" onClick="document.calculator.ans.value+='2'" />
-        <input type="button" value="3" onClick="document.calculator.ans.value+='3'" />
-        <input type="button" value="+" onClick="document.calculator.ans.value+='+'" />
-        <br />
-        <input type="button" value="4" onClick="document.calculator.ans.value+='4'" />
-        <input type="button" value="5" onClick="document.calculator.ans.value+='5'" />
-        <input type="button" value="6" onClick="document.calculator.ans.value+='6'" />
-        <input type="button" value="-" onClick="document.calculator.ans.value+='-'" />
-        <br />
-        <input type="button" value="7" onClick="document.calculator.ans.value+='7'" />
-        <input type="button" value="8" onClick="document.calculator.ans.value+='8'" />
-        <input type="button" value="9" onClick="document.calculator.ans.value+='9'" />
-        <input type="button" value="*" onClick="document.calculator.ans.value+='*'" />
-        <input type="button" value="/" onClick="document.calculator.ans.value+='/'" />
-        <br />
-        <input type="button" value="0" onClick="document.calculator.ans.value+='0'" />
-        <input type="reset" value="Reset" />
-        <input type="button" value="="
-         onClick="document.calculator.ans.value=eval(document.calculator.ans.value)" /> */}
+        <Tile sbstyle={ style } onClick={ clear }>C</Tile>
+        <Screen sbstyle={ style }>{ result || 'Enter a number' }</Screen>
+
+        <Tile sbstyle={ style } onClick={ () => addToValue('7') }>7</Tile>
+        <Tile sbstyle={ style } onClick={ () => addToValue('8') }>8</Tile>
+        <Tile sbstyle={ style } onClick={ () => addToValue('9') }>9</Tile>
+        <Tile sbstyle={ style } onClick={ () => setOperator('+') }>+</Tile>
+        <Tile sbstyle={ style } onClick={ () => addToValue('4') }>4</Tile>
+        <Tile sbstyle={ style } onClick={ () => addToValue('5') }>5</Tile>
+        <Tile sbstyle={ style } onClick={ () => addToValue('6') }>6</Tile>
+        <Tile sbstyle={ style } onClick={ () => setOperator('-') }>-</Tile>
+        <Tile sbstyle={ style } onClick={ () => addToValue('1') }>1</Tile>
+        <Tile sbstyle={ style } onClick={ () => addToValue('2') }>2</Tile>
+        <Tile sbstyle={ style } onClick={ () => addToValue('3') }>3</Tile>
+        <Tile sbstyle={ style } onClick={ () => setOperator('/') }>/</Tile>
+        <Tile sbstyle={ style } onClick={ () => addToValue('0') }>0</Tile>
+        <Tile sbstyle={ style } onClick={ () => addToValue('.') }>.</Tile>
+        <Tile sbstyle={ style } onClick={ triggerCalculation }>=</Tile>
+        <Tile sbstyle={ style } onClick={ () => setOperator('*') }>*</Tile>
       </div>
-      <pre>
-        {
-          JSON.stringify({ ...props, style }, null, 2)
-        }
-      </pre>
     </div>
   </div>
 )
@@ -133,7 +85,7 @@ const defaultStyles = {
     alignItems: 'baseline',
   },
   inner: {
-    width: '100%',
+    width: 260,
     marginTop: 60,
     display: 'flex',
     flexDirection: 'column',
@@ -145,10 +97,22 @@ const defaultStyles = {
     borderRadius: 8,
     border: 'none',
     backgroundColor: '#3a3a3a',
-    margin: '0 4px 10px 8px',
+    margin: 5,
     height: 40,
     width: 50,
-    boxShadow: '0px 3px 0px 0px #222121, inset -1px -3px 10px 1px #515151',
+    // boxShadow: '0px 3px 0px 0px #222121, inset -1px -3px 10px 1px #515151',
+    outline: 'none',
+  },
+  screen: {
+    color: 'black',
+    borderRadius: 8,
+    border: 'none',
+    backgroundColor: '#ccc',
+    margin: 5,
+    padding: 5,
+    height: 40,
+    width: 170,
+    textAlign: 'right',
     outline: 'none',
   },
 }
@@ -157,8 +121,44 @@ export default compose(
   getContext({
     appColor: () => null,
   }),
-  substyle(defaultStyles, (props) => {
-    console.log(props)
+  substyle(defaultStyles),
+  withState('result', 'setResult', ''),
+  withState('value', 'setValue', 0),
+  withState('secondValue', 'setSecondValue', 0),
+  withState('operator', 'setOperator', null),
+  withHandlers({
+    addToValue: ({ setValue, value, result, operator, secondValue, setSecondValue, setResult }) =>
+      (digit) => {
+        // missing double 0 starting, multiple dots
+        if (operator === null) {
+          setValue(`${value}${digit}`, () => setResult(`${result}${digit}`))
+        } else {
+          setSecondValue(`${secondValue}${digit}`, () => setResult(`${result}${digit}`))
+        }
+      },
+    triggerCalculation:
+    ({ operator, value, secondValue, setSecondValue, setOperator, setValue, setResult }) =>
+      () => {
+        if ( !operator || !value || !secondValue ) return
+        const nVal = calculate[operator](Number(value), Number(secondValue))
+        setValue(nVal, () => setSecondValue('', () => setResult(nVal, () => setOperator(null))))
+      },
+    clear: ({ setOperator, setValue, setResult, setSecondValue }) =>
+      () => {
+        setValue('', () => setSecondValue('', () => setResult('', () => setOperator(null))))
+      },
+    setOperator: ({ operator, result, setResult, setOperator, secondValue }) => (o) => {
+      if ( result && !secondValue ) {
+        setOperator(o, () => {
+          if ( o !== null ) {
+            let newRes = `${result} ${o} `
+            if ( operator !== null ) {
+              newRes = result.replace(operator, o)
+            }
+            setResult(newRes)
+          }
+        })
+      }
+    },
   }),
-  withState('result', 'setResult', 0),
 )(JavaScriptCalculator)
