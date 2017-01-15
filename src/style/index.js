0565ff3f609b639b40810ef05d10eb1d9d5be71e
Julius Breckel <julius.breckel@signavio.com>
2017-01-15 22:15:14 +0300

    add global style folder

---
+++ src/style/index.js
@@ -0,0 +1,26 @@
+export const ContainerDarkStyle = {
+  outer: {
+    minHeight: '100%',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'baseline',
+  },
+  inner: {
+    color: 'white',
+    width: '100%',
+    marginTop: 60,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}
+
+export const DisclaimerOnDarkStyle = {
+  color: 'white',
+  border: '1px solid white',
+  borderRadius: 4,
+  padding: 7,
+  margin: 7,
+}
